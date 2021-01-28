97b1db6d8930abb6ed0947ac37d77f44389c3e78
riyaagarwal010 <riyaagarwal0402@gmail.com>
2021-01-29 00:34:16 +0530

    All setup

--- public/js/app.js
+++ public/js/app.js
@@ -13,10 +13,10 @@ const messageTwo=document.querySelector('#Two')
 weatherForm.addEventListener('submit',(e)=>{
    e.preventDefault()
    const location=search.value
-   messageOne.textContent='Loading'
+   messageOne.textContent='Loading....'
    messageTwo.textContent=''
 
-   fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+   fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error)
         messageOne.textContent=data.error
--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const viewsPath=path.join(__dirname,'../templates/views')
 const partialsPath=path.join(__dirname,'../templates/partials')
 
 const app=express()
+const port = process.env.PORT || 3000
 app.set('view engine','hbs')
 app.use(express.static(path.join(__dirname,'../public')))
 app.set('views',viewsPath)
@@ -84,6 +85,6 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log('Server is on port 3000')
+app.listen(port,()=>{
+    console.log('Server is on port '+ port)
 })
